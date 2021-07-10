2ec82d43b11b90a42401c5c63345a80bf0266515
Lucas Souza <lucasp.sdev@gmail.com>
2021-07-10 11:11:07 -0300

    Projeto concluído ✔️

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import './styles/global.scss';
+import "./styles/global.scss";
 
-import './styles/sidebar.scss';
-import './styles/content.scss';
-import { MoviesProvider } from './MoviesContext';
-import { SideBar } from './components/SideBar';
-import { Content } from './components/Content';
-
+import "./styles/sidebar.scss";
+import "./styles/content.scss";
+import { MoviesProvider } from "./MoviesContext";
+import { SideBar } from "./components/SideBar";
+import { Content } from "./components/Content";
 
 export function App() {
-
   return (
     <MoviesProvider>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-          <SideBar />
-          <Content />
+      <div style={{ display: "flex", flexDirection: "row" }}>
+        <SideBar />
+        <Content />
       </div>
     </MoviesProvider>
-  )
+  );
 }
--- src/MoviesContext.tsx
+++ src/MoviesContext.tsx
@@ -1,9 +1,15 @@
-import { createContext, ReactNode, useContext, useEffect, useState } from 'react';
-import {api} from './services/api'
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { api } from "./services/api";
 
 interface GenreResponseProps {
   id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
   title: string;
 }
 
@@ -18,41 +24,46 @@ interface MovieProps {
 }
 
 interface MoviesProviderProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
 interface MoviesContextData {
   genres: GenreResponseProps[];
-  selectedGenre:GenreResponseProps;
-  selectedGenreId:number;
+  selectedGenre: GenreResponseProps;
+  selectedGenreId: number;
   movies: MovieProps[];
-  handleClickButton: (id:number) => void;
+  handleClickButton: (id: number) => void;
 }
 
-const MoviesContext = createContext<MoviesContextData>(
-  {} as MoviesContextData)
+const MoviesContext = createContext<MoviesContextData>({} as MoviesContextData);
 
-export function MoviesProvider({children}: MoviesProviderProps) {
+export function MoviesProvider({ children }: MoviesProviderProps) {
   const [genres, setGenres] = useState<GenreResponseProps[]>([]);
   const [selectedGenreId, setSelectedGenreId] = useState(1);
 
   const [movies, setMovies] = useState<MovieProps[]>([]);
-  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>(
+    {} as GenreResponseProps
+  );
 
   useEffect(() => {
-    api.get<GenreResponseProps[]>('genres').then(response => {
+    api.get<GenreResponseProps[]>("genres").then((response) => {
       setGenres(response.data);
     });
   }, []);
 
   useEffect(() => {
-    api.get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`).then(response => {
-      setMovies(response.data);
-    });
+    api
+      .get<MovieProps[]>(`movies/?Genre_id=${selectedGenreId}`)
+      .then((response) => {
+        setMovies(response.data);
+      });
 
-    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
-      setSelectedGenre(response.data);
-    })
+    api
+      .get<GenreResponseProps>(`genres/${selectedGenreId}`)
+      .then((response) => {
+        setSelectedGenre(response.data);
+      });
   }, [selectedGenreId]);
 
   function handleClickButton(id: number) {
@@ -60,14 +71,22 @@ export function MoviesProvider({children}: MoviesProviderProps) {
   }
 
   return (
-    <MoviesContext.Provider value={{genres, selectedGenreId, selectedGenre, handleClickButton, movies}}>
+    <MoviesContext.Provider
+      value={{
+        genres,
+        selectedGenreId,
+        selectedGenre,
+        handleClickButton,
+        movies,
+      }}
+    >
       {children}
     </MoviesContext.Provider>
-  )
+  );
 }
 
 export function useMovies() {
   const context = useContext(MoviesContext);
 
-  return context
+  return context;
 }
